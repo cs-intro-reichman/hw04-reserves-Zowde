ff4a048331eb89e34632185dddb130ad188dd0cc
Zowde <157274222+Zowde@users.noreply.github.com>
2024-02-16 12:12:17 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -1,3 +1,5 @@
+import javax.lang.model.util.ElementScanner14;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
     //////                                               ///////
@@ -21,22 +23,92 @@ public class StringOps {
     //////        in Recitation 3 question 5             ///////
     //////                                               ///////
     ////////////////////////////////////////////////////////////
-    public static void main(String[] args) {
-        
-    }
+  
 
     public static String capVowelsLowRest (String string) {
         // Write your code here:
-        return "";
+        string=tolowercase(string);
+        String comparison="aeiou";
+        String news="";
+        for(int i=0;i<string.length();i++)
+        {
+            if(comparison.indexOf(string.charAt(i))!=-1)
+            {
+                news+=(char)(string.charAt(i)-32);
+            }
+            else
+            {
+                news+=string.charAt(i);
+            }
+        }
+;        return news;
     }
+     public static String tolowercase(String word)
+     {
+         String mila="";
+         for(int i=0;i<word.length();i++)
+         {
+          if(word.charAt(i)>='A'&&word.charAt(i)<='Z')
+          {
+            mila+=(char)(word.charAt(i)+32);
 
+         }
+         else
+         {
+            mila+=word.charAt(i);
+         }
+         }
+            return mila;
+     }
     public static String camelCase (String string) {
         // Write your code here:
-        return "";
+        string =tolowercase(string);//convert the letters to lower case
+        String clonestring="",word="";//clonestring-will compose the result,
+       int count=0;//differentiate the first word from the others
+        for(int i=0;i<string.length();i++)
+        {
+            while(i<string.length()&&string.charAt(i)!=' ')
+            {
+               word+=string.charAt(i);
+               i++;
+            }
+            if(word!="")
+            {
+                if(count>0)
+                word=CapThefirst(word);
+                 clonestring+=word;
+                count++;
+            word="";
+            }
+        }
+
+
+        return clonestring;
+    }
+    public static String CapThefirst(String word)//change the first letter to upper case
+    {
+        String cloneword=(char)(word.charAt(0)-32)+word.substring(1);
+        return cloneword;
     }
 
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
-        return new int[1];
+        int len=0;
+        String index="";
+        for(int i=0;i<string.length();i++)
+        {
+            if(string.charAt(i)==chr)
+            {
+                 len++;
+                 index+=i;
+            }
+        }
+        int [] arr=new int[len];
+        for(int i=0;i<arr.length;i++)
+        {
+              arr[i]=index.charAt(i)-'0';
+        }
+
+        return arr;
     }
 }
